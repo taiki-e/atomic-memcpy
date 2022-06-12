28b0277dbfcfaa1cb9d3de811a12ee1cbd06a862
Taiki Endo <te316e89@gmail.com>
2022-06-12 23:06:17 +0900

    Update tools/build.sh

--- tools/build.sh
+++ tools/build.sh
@@ -7,7 +7,7 @@ trap -- 'exit 0' SIGINT
 
 default_targets=(
     # no atomic load/store (16-bit)
-    # avr-unknown-gnu-atmega328 # https://github.com/rust-lang/compiler-builtins/issues/400
+    avr-unknown-gnu-atmega328
     msp430-none-elf
     # no atomic load/store (32-bit)
     riscv32i-unknown-none-elf
@@ -23,9 +23,9 @@ default_targets=(
     riscv32imc-esp-espidf
 )
 
-common_args=()
+pre_args=()
 if [[ "${1:-}" == "+"* ]]; then
-    common_args+=("$1")
+    pre_args+=("$1")
     shift
 fi
 if [[ $# -gt 0 ]]; then
@@ -34,11 +34,14 @@ else
     targets=("${default_targets[@]}")
 fi
 
-rustup_target_list=$(rustup ${common_args[@]+"${common_args[@]}"} target list)
-rustc_target_list=$(rustc ${common_args[@]+"${common_args[@]}"} --print target-list)
-rustc_version=$(rustc ${common_args[@]+"${common_args[@]}"} -Vv | grep 'release: ' | sed 's/release: //')
+rustup_target_list=$(rustup ${pre_args[@]+"${pre_args[@]}"} target list)
+rustc_target_list=$(rustc ${pre_args[@]+"${pre_args[@]}"} --print target-list)
+rustc_version=$(rustc ${pre_args[@]+"${pre_args[@]}"} -Vv | grep 'release: ' | sed 's/release: //')
+subcmd=build
+nightly=''
 if [[ "${rustc_version}" == *"nightly"* ]] || [[ "${rustc_version}" == *"dev"* ]]; then
-    rustup ${common_args[@]+"${common_args[@]}"} component add rust-src &>/dev/null
+    nightly=1
+    rustup ${pre_args[@]+"${pre_args[@]}"} component add rust-src &>/dev/null
     case "${rustc_version}" in
         # -Z check-cfg requires 1.63.0-nightly
         1.[0-5]* | 1.6[0-2].*) ;;
@@ -47,6 +50,7 @@ if [[ "${rustc_version}" == *"nightly"* ]] || [[ "${rustc_version}" == *"dev"* ]
             ;;
     esac
 fi
+echo "base rustflags='${RUSTFLAGS:-} ${check_cfg:-}'"
 
 x() {
     local cmd="$1"
@@ -59,29 +63,27 @@ x() {
 build() {
     local target="$1"
     shift
-    args=()
+    local args=()
+    local target_rustflags="${RUSTFLAGS:-} ${check_cfg:-}"
     if ! grep <<<"${rustc_target_list}" -Eq "^${target}$"; then
         echo "target '${target}' not available on ${rustc_version}"
         return 0
     fi
     if [[ "${target}" == "avr-"* ]]; then
-        # https://github.com/rust-lang/compiler-builtins/issues/400
-        case "${rustc_version}" in
-            1.4* | 1.50.* | 1.51.*) ;;
-            *) return 0 ;;
-        esac
+        # https://github.com/rust-lang/rust/issues/88252
+        target_rustflags="${target_rustflags} -C opt-level=s"
     fi
     if [[ "${target}" == "riscv32"* ]]; then
         # TODO: cfg(target_has_atomic) requires Rust 1.60.
         case "${rustc_version}" in
-            1.3* | 1.4* | 1.5*) return 0 ;;
+            1.[0-5]*) return 0 ;;
             *) ;;
         esac
     fi
-    args+=(${common_args[@]+"${common_args[@]}"} hack build)
+    args+=(${pre_args[@]+"${pre_args[@]}"} hack "${subcmd}")
     if grep <<<"${rustup_target_list}" -Eq "^${target}( |$)"; then
-        x rustup ${common_args[@]+"${common_args[@]}"} target add "${target}" &>/dev/null
-    elif [[ "${rustc_version}" == *"nightly"* ]] || [[ "${rustc_version}" == *"dev"* ]]; then
+        x rustup ${pre_args[@]+"${pre_args[@]}"} target add "${target}" &>/dev/null
+    elif [[ -n "${nightly}" ]]; then
         case "${target}" in
             *-none* | avr-* | *-esp-espidf) args+=(-Z build-std=core) ;;
             *) args+=(-Z build-std) ;;
@@ -95,14 +97,15 @@ build() {
     fi
     args+=(--target "${target}")
 
-    RUSTFLAGS="${RUSTFLAGS:-} ${check_cfg:-}" \
-        x cargo "${args[@]}" --feature-powerset --optional-deps --no-dev-deps --manifest-path Cargo.toml
-    RUSTFLAGS="${RUSTFLAGS:-} ${check_cfg:-}" \
-        x cargo "${args[@]}" --release --feature-powerset --optional-deps --no-dev-deps --manifest-path Cargo.toml
-    RUSTFLAGS="${RUSTFLAGS:-} ${check_cfg:-}" \
+    RUSTFLAGS="${target_rustflags}" \
         x cargo "${args[@]}" --manifest-path tests/no-std/Cargo.toml
-    RUSTFLAGS="${RUSTFLAGS:-} ${check_cfg:-}" \
+    RUSTFLAGS="${target_rustflags}" \
         x cargo "${args[@]}" --release --manifest-path tests/no-std/Cargo.toml
+
+    RUSTFLAGS="${target_rustflags}" \
+        x cargo "${args[@]}" --feature-powerset --optional-deps --no-dev-deps --manifest-path Cargo.toml
+    RUSTFLAGS="${target_rustflags}" \
+        x cargo "${args[@]}" --release --feature-powerset --optional-deps --no-dev-deps --manifest-path Cargo.toml
 }
 
 for target in "${targets[@]}"; do
