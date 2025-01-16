5e4a063508c78ee725d07af49652d449c739af24
Taiki Endo <te316e89@gmail.com>
2025-01-16 21:39:02 +0900

    tools: Format shell scripts with indent_size = 2 to match scripts in CI config

--- tools/build.sh
+++ tools/build.sh
@@ -10,55 +10,55 @@ cd -- "$(dirname -- "$0")"/..
 #    ./tools/build.sh [+toolchain] [target]...
 
 default_targets=(
-    # no atomic load/store (16-bit)
-    msp430-none-elf
+  # no atomic load/store (16-bit)
+  msp430-none-elf
 
-    # no atomic CAS (16-bit)
-    avr-unknown-gnu-atmega328
-    # no atomic CAS (32-bit)
-    thumbv6m-none-eabi
-    riscv32i-unknown-none-elf
+  # no atomic CAS (16-bit)
+  avr-unknown-gnu-atmega328
+  # no atomic CAS (32-bit)
+  thumbv6m-none-eabi
+  riscv32i-unknown-none-elf
 
-    # no-std
-    thumbv7m-none-eabi
-    # riscv32 with atomic
-    riscv32imac-unknown-none-elf
-    riscv32imc-esp-espidf
+  # no-std
+  thumbv7m-none-eabi
+  # riscv32 with atomic
+  riscv32imac-unknown-none-elf
+  riscv32imc-esp-espidf
 )
 
 x() {
-    (
-        set -x
-        "$@"
-    )
+  (
+    set -x
+    "$@"
+  )
 }
 x_cargo() {
-    if [[ -n "${RUSTFLAGS:-}" ]]; then
-        printf '%s\n' "+ RUSTFLAGS='${RUSTFLAGS}' \\"
-    fi
-    x cargo "$@"
-    printf '\n'
+  if [[ -n "${RUSTFLAGS:-}" ]]; then
+    printf '%s\n' "+ RUSTFLAGS='${RUSTFLAGS}' \\"
+  fi
+  x cargo "$@"
+  printf '\n'
 }
 retry() {
-    for i in {1..10}; do
-        if "$@"; then
-            return 0
-        else
-            sleep "${i}"
-        fi
-    done
-    "$@"
+  for i in {1..10}; do
+    if "$@"; then
+      return 0
+    else
+      sleep "${i}"
+    fi
+  done
+  "$@"
 }
 
 pre_args=()
 if [[ "${1:-}" == "+"* ]]; then
-    pre_args+=("$1")
-    shift
+  pre_args+=("$1")
+  shift
 fi
 if [[ $# -gt 0 ]]; then
-    targets=("$@")
+  targets=("$@")
 else
-    targets=("${default_targets[@]}")
+  targets=("${default_targets[@]}")
 fi
 
 rustup_target_list=$(rustup ${pre_args[@]+"${pre_args[@]}"} target list | cut -d' ' -f1)
@@ -71,49 +71,49 @@ llvm_version="${llvm_version%%.*}"
 base_args=(${pre_args[@]+"${pre_args[@]}"} hack build)
 nightly=''
 if [[ "${rustc_version}" =~ nightly|dev ]]; then
-    nightly=1
-    retry rustup ${pre_args[@]+"${pre_args[@]}"} component add rust-src &>/dev/null
+  nightly=1
+  retry rustup ${pre_args[@]+"${pre_args[@]}"} component add rust-src &>/dev/null
 fi
 
 build() {
-    local target="$1"
-    shift
-    local args=("${base_args[@]}" --target "${target}")
-    local target_rustflags="${RUSTFLAGS:-}"
-    if ! grep -Eq "^${target}$" <<<"${rustc_target_list}"; then
-        printf '%s\n' "target '${target}' not available on ${rustc_version} (skipped all checks)"
-        return 0
+  local target="$1"
+  shift
+  local args=("${base_args[@]}" --target "${target}")
+  local target_rustflags="${RUSTFLAGS:-}"
+  if ! grep -Eq "^${target}$" <<<"${rustc_target_list}"; then
+    printf '%s\n' "target '${target}' not available on ${rustc_version} (skipped all checks)"
+    return 0
+  fi
+  if grep -Eq "^${target}$" <<<"${rustup_target_list}"; then
+    retry rustup ${pre_args[@]+"${pre_args[@]}"} target add "${target}" &>/dev/null
+  elif [[ -n "${nightly}" ]]; then
+    # Only build core because our library code doesn't depend on std.
+    args+=(-Z build-std="core")
+  else
+    printf '%s\n' "target '${target}' requires nightly compiler (skipped all checks)"
+    return 0
+  fi
+  if [[ "${target}" == "avr"* ]]; then
+    if [[ "${llvm_version}" -eq 16 ]]; then
+      # https://github.com/rust-lang/compiler-builtins/issues/523
+      target_rustflags+=" -C linker-plugin-lto -C codegen-units=1"
+    elif [[ "${llvm_version}" -ge 17 ]]; then
+      # https://github.com/rust-lang/rust/issues/88252
+      target_rustflags+=" -C opt-level=s"
     fi
-    if grep -Eq "^${target}$" <<<"${rustup_target_list}"; then
-        retry rustup ${pre_args[@]+"${pre_args[@]}"} target add "${target}" &>/dev/null
-    elif [[ -n "${nightly}" ]]; then
-        # Only build core because our library code doesn't depend on std.
-        args+=(-Z build-std="core")
-    else
-        printf '%s\n' "target '${target}' requires nightly compiler (skipped all checks)"
-        return 0
-    fi
-    if [[ "${target}" == "avr"* ]]; then
-        if [[ "${llvm_version}" -eq 16 ]]; then
-            # https://github.com/rust-lang/compiler-builtins/issues/523
-            target_rustflags+=" -C linker-plugin-lto -C codegen-units=1"
-        elif [[ "${llvm_version}" -ge 17 ]]; then
-            # https://github.com/rust-lang/rust/issues/88252
-            target_rustflags+=" -C opt-level=s"
-        fi
-    fi
+  fi
 
-    RUSTFLAGS="${target_rustflags}" \
-        x_cargo "${args[@]}" --manifest-path tests/no-std/Cargo.toml
-    RUSTFLAGS="${target_rustflags}" \
-        x_cargo "${args[@]}" --manifest-path tests/no-std/Cargo.toml --release
+  RUSTFLAGS="${target_rustflags}" \
+    x_cargo "${args[@]}" --manifest-path tests/no-std/Cargo.toml
+  RUSTFLAGS="${target_rustflags}" \
+    x_cargo "${args[@]}" --manifest-path tests/no-std/Cargo.toml --release
 
-    RUSTFLAGS="${target_rustflags}" \
-        x_cargo "${args[@]}" --feature-powerset --optional-deps --no-dev-deps --manifest-path Cargo.toml
-    RUSTFLAGS="${target_rustflags}" \
-        x_cargo "${args[@]}" --feature-powerset --optional-deps --no-dev-deps --manifest-path Cargo.toml --release
+  RUSTFLAGS="${target_rustflags}" \
+    x_cargo "${args[@]}" --feature-powerset --optional-deps --no-dev-deps --manifest-path Cargo.toml
+  RUSTFLAGS="${target_rustflags}" \
+    x_cargo "${args[@]}" --feature-powerset --optional-deps --no-dev-deps --manifest-path Cargo.toml --release
 }
 
 for target in "${targets[@]}"; do
-    build "${target}"
+  build "${target}"
 done
